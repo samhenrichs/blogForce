4b0a452f2f75af9fe80a843bd7fa45ebea8b75b7
samhenrichs <eurostepedits@gmail.com>
2016-05-14 16:14:07 -0500

    Almost Done

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,5 @@
 class Link < ActiveRecord::Base
-  has_attached_file :image
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# set the path in general, might not be necessary
+ENV['PATH'] = '/usr/local/bin:' + ENV['PATH']
