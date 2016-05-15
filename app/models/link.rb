43aff92d77498358710d3cbbe050752adf0e76ea
samhenrichs <eurostepedits@gmail.com>
2016-05-15 15:04:30 -0500

    Added dropbox

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+      :storage => :dropbox,
+      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
   validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
 
 end
