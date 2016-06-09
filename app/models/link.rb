e1b6112f34836cda9ac367d955009e6d9022c0c7
samhenrichs <eurostepedits@gmail.com>
2016-06-09 07:29:36 -0500

    a

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,8 +2,7 @@ class Link < ActiveRecord::Base
 
   extend FriendlyId
   friendly_id :product, use: :slugged
-  attr_accessor :slug
-
+  
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
       :storage => :dropbox,
       :dropbox_credentials => Rails.root.join("config/dropbox.yml")
