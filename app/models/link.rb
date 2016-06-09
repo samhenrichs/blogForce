e5eaf15c859ae108f0f536719a4d82d10ef18692
samhenrichs <eurostepedits@gmail.com>
2016-06-09 08:03:48 -0500

    a

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ActiveRecord::Base
 
   extend FriendlyId
   friendly_id :product, use: :slugged
-  
+
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
       :storage => :dropbox,
       :dropbox_credentials => Rails.root.join("config/dropbox.yml")
@@ -10,4 +10,8 @@ class Link < ActiveRecord::Base
   validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
   validates :url, presence: true
 
+  def should_generate_new_friendly_id?
+  new_record? || slug.blank?
+  end
+  
 end
